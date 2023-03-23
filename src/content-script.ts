9f6ae9c704e9abc1941c5770f2072b28deb9271b
Kamil Szczerba <klavinski@users.noreply.github.com>
2023-03-23 13:50:23 +0100

    Add models

--- src/content-script.ts
+++ src/content-script.ts
@@ -2,10 +2,32 @@ import * as browser from "webextension-polyfill";
 const src = browser.runtime.getURL('src/sandbox.html')
 const sb = browser.runtime.getURL('src/sandbox.ts')
 console.log(sb)
+window.addEventListener('message', message => {
+   //console.log(message)
+    message.source?.postMessage({ from: "content-script" })
+})
 const iframe = document.createElement('iframe')
 iframe.src = src
 iframe.className = "crx"
-iframe.onload = () => { iframe.contentWindow?.postMessage({ type: "init", src: sb }, "*")
-}
+setTimeout(() => { 
+    const contentWindow = iframe.contentWindow
+    if (contentWindow) 
+    contentWindow.postMessage({ type: "test", src: sb }, "*")
+    else
+    console.log("no content window", iframe)
+    }, 5000)
+
+iframe.style.display = "none"
 document.body.appendChild(iframe)
-console.log(iframe)
+console.log(iframe.contentWindow)
+if ( iframe.contentWindow ) {
+    iframe.contentWindow.postMessage({ type: "test", src: sb }, "*")
+} else
+console.log("no content window", iframe)
+
+import { Readability } from "@mozilla/readability"
+browser.runtime.onConnect.addListener(port => {
+    console.log(port)
+})
+const doc = new Readability(document).parse()
+console.log(doc)
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,12 +1,23 @@
 import { pipeline } from "@xenova/transformers"
 
 const run = async () => {
-console.log(await pipeline('question-answering'))
+    const qa = await pipeline('question-answering', 'distilbert-base-cased-distilled-squad')
+    console.log(await qa('Is X a man or a woman?', `X is Alice's father.`))
+    // const textGeneration = await pipeline('text-generation', 'distilgpt2')
+    // console.log(await textGeneration('Who is sitting next to Bob?'))
+    // const embeddings = await pipeline('embeddings', 'sentence-transformers/all-distilroberta-v1')
+    // console.log(await embeddings('Who is sitting next to Bob?'))
+    const summarization = await pipeline('summarization', 'facebook/bart-large-cnn')
+    console.log(await summarization(`In order to do something interesting with the sandboxed file, we need to load it in a context where it can be addressed by the extension's code. Here, sandbox.html has been loaded into the extension's Event Page (eventpage.html) via an iframe. eventpage.js contains code that sends a message into the sandbox whenever the browser action is clicked by finding the iframe on the page, and executing the postMessage method on its contentWindow. The message is an object containing two properties: context and command. We'll dive into both in a moment.`))
 }
+window.addEventListener('message', message => {
+    //console.log(message)
+    message.source?.postMessage({ from: "sandbox" })
+})
 run()
 if ( document ) {
     let a: number;
     a = 6;
     console.log("ok", a)
-document.body.textContent= "Hello World";
+    document.body.textContent= "Hello World";
 }
