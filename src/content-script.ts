5caf7c4a1cc8bf4ee5a2b3d3de8d054c0e6a6c3d
Kamil Szczerba <klavinski@users.noreply.github.com>
2023-03-25 15:26:52 +0100

    Work offline

--- src/content-script.ts
+++ src/content-script.ts
@@ -1,33 +1,31 @@
-import * as browser from "webextension-polyfill";
-const src = browser.runtime.getURL('src/sandbox.html')
-const sb = browser.runtime.getURL('src/sandbox.ts')
-console.log(sb)
-window.addEventListener('message', message => {
-   //console.log(message)
-    message.source?.postMessage({ from: "content-script" })
-})
-const iframe = document.createElement('iframe')
+import * as browser from "webextension-polyfill"
+const src = browser.runtime.getURL( "src/sandbox.html" )
+const sb = browser.runtime.getURL( "src/sandbox.ts" )
+console.log( sb )
+window.addEventListener( "message", message => {
+    console.log( message )
+    message.source?.postMessage( { from: "content-script" } )
+} )
+const iframe = document.createElement( "iframe" )
 iframe.src = src
 iframe.className = "crx"
-setTimeout(() => { 
-    const contentWindow = iframe.contentWindow
-    if (contentWindow) 
-    contentWindow.postMessage({ type: "test", src: sb }, "*")
-    else
-    console.log("no content window", iframe)
-    }, 5000)
 
-iframe.style.display = "none"
-document.body.appendChild(iframe)
-console.log(iframe.contentWindow)
-if ( iframe.contentWindow ) {
-    iframe.contentWindow.postMessage({ type: "test", src: sb }, "*")
-} else
-console.log("no content window", iframe)
+// iframe.style.display = "none"
+document.body.appendChild( iframe )
+console.log( iframe.contentWindow )
+iframe.onload = () => {
+    if ( iframe.contentWindow ) {
+        console.log( "content window", iframe )
+        iframe.contentWindow.postMessage( "test", "*" )
+    } else
+        console.log( "no content window", iframe )
+}
+
 
 import { Readability } from "@mozilla/readability"
-browser.runtime.onConnect.addListener(port => {
-    console.log(port)
-})
-const doc = new Readability(document).parse()
-console.log(doc)
+const doc = new Readability( document ).parse()
+console.log( doc )
+
+browser.runtime.onMessage.addListener( ( message, sender ) => {
+    console.log( message, sender )
+} )
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,23 +1,36 @@
 import { pipeline } from "@xenova/transformers"
 
+const phrases = [
+    "Canine nutrition",
+    "Dog mood",
+    "Hot dog",
+]
+const query = "Dog food"
+
 const run = async () => {
-    const qa = await pipeline('question-answering', 'distilbert-base-cased-distilled-squad')
-    console.log(await qa('Is X a man or a woman?', `X is Alice's father.`))
+    const qa = await pipeline( "question-answering", "distilbert-base-cased-distilled-squad" )
+    console.log( await qa( "Is X a man or a woman?", "X is Alice's father." ) )
     // const textGeneration = await pipeline('text-generation', 'distilgpt2')
     // console.log(await textGeneration('Who is sitting next to Bob?'))
     // const embeddings = await pipeline('embeddings', 'sentence-transformers/all-distilroberta-v1')
     // console.log(await embeddings('Who is sitting next to Bob?'))
-    const summarization = await pipeline('summarization', 'facebook/bart-large-cnn')
-    console.log(await summarization(`In order to do something interesting with the sandboxed file, we need to load it in a context where it can be addressed by the extension's code. Here, sandbox.html has been loaded into the extension's Event Page (eventpage.html) via an iframe. eventpage.js contains code that sends a message into the sandbox whenever the browser action is clicked by finding the iframe on the page, and executing the postMessage method on its contentWindow. The message is an object containing two properties: context and command. We'll dive into both in a moment.`))
+    // const summarization = await pipeline( "summarization", "facebook/bart-large-cnn" )
+    // console.log( await summarization( "In order to do something interesting with the sandboxed file, we need to load it in a context where it can be addressed by the extension's code. Here, sandbox.html has been loaded into the extension's Event Page (eventpage.html) via an iframe. eventpage.js contains code that sends a message into the sandbox whenever the browser action is clicked by finding the iframe on the page, and executing the postMessage method on its contentWindow. The message is an object containing two properties: context and command. We'll dive into both in a moment." ) )
 }
-window.addEventListener('message', message => {
-    //console.log(message)
-    message.source?.postMessage({ from: "sandbox" })
-})
+window.addEventListener( "message", message => {
+    console.log( message, message.source === window, message.target === window, message.currentTarget === window )
+    // message.source?.postMessage({ from: "sandbox" })
+} )
 run()
 if ( document ) {
-    let a: number;
-    a = 6;
-    console.log("ok", a)
-    document.body.textContent= "Hello World";
+    let a: number
+    a = 6
+    console.log( "ok", a )
+    document.body.textContent = "Hello World"
 }
+
+setInterval( function() {
+    // Send the message "Hello" to the parent window
+    // ...if the domain is still "davidwalsh.name"
+    parent.postMessage( "Hello", "http://davidwalsh.name" )
+}, 3000 )
