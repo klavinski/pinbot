0586f7e14b6c6d13a948b7fcd58909ee75c6724b
Kamil Szczerba <klavinski@users.noreply.github.com>
2023-05-27 11:32:35 +0200

    Remove cache

--- src/embed.ts
+++ src/embed.ts
@@ -9,7 +9,7 @@ const sandbox = new Promise<Window>( ( resolve, reject ) => {
 } )
 iframe.src = browser.runtime.getURL( "src/sandbox.html" )
 
-const embedWithoutCache = ( text: string ) => new Promise<Float32Array>( async resolve => {
+export const embed = ( text: string ) => new Promise<Float32Array>( async resolve => {
     const controller = new AbortController()
     window.addEventListener( "message", ( { data } ) => {
         const parsing = z.object( { text: z.literal( text ), embeddings: z.instanceof( Float32Array ) } ).safeParse( data )
@@ -20,11 +20,3 @@ const embedWithoutCache = ( text: string ) => new Promise<Float32Array>( async r
     }, { signal: controller.signal } );
     ( await sandbox ).postMessage( text, "*" )
 } )
-
-const cache = new Map<string, Float32Array>()
-
-export const embed = async ( text: string ) => {
-    if ( ! cache.has( text ) )
-        cache.set( text, await embedWithoutCache( text ) )
-    return cache.get( text )!
-}
